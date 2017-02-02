380cee6aa3555d6b6a0dd6edc5022b6853d4169d
Nicholas Rogoff <nicholas@rogoff.uk.com>
2017-02-02 12:13:25 +0000

    Fixed allow nulls in Group audit table

--- src/Database/EntAppSettingsDb/EntAppSettingsDb/dbo/AppSettingGroup_Audit.sql
+++ src/Database/EntAppSettingsDb/EntAppSettingsDb/dbo/AppSettingGroup_Audit.sql
@@ -2,9 +2,9 @@
 (
     [AuditId] BIGINT IDENTITY (1, 1) NOT NULL,
 	[AppSettingGroupId] INT NOT NULL,
-    [ParentGroupId] INT NOT NULL, 
+    [ParentGroupId] INT NULL, 
 	[Group] VARCHAR(50) NOT NULL,
-	[Description] NVARCHAR(1000) NOT NULL,
+	[Description] NVARCHAR(1000) NULL,
 	[ModifiedDate] DATETIME        DEFAULT (getutcdate()) NOT NULL,
     [ModifiedBy]   NVARCHAR (50)   DEFAULT (suser_sname()) NOT NULL, 
     CONSTRAINT [PK_AppSettingGroup_Audit] PRIMARY KEY ([AuditId]), 
