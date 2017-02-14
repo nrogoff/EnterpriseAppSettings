aad0bea9bde9ac8034e1ff2c57f4ecdb15bd4e75
Nicholas Rogoff <nicholas@rogoff.uk.com>
2017-02-14 21:42:36 +0000

    updated post build script

--- src/dotNET/hms.entappsettings.webapi/hms.entappsettings.webapi/BuildScripts/CreateLocalDbInstance.bat
+++ src/dotNET/hms.entappsettings.webapi/hms.entappsettings.webapi/BuildScripts/CreateLocalDbInstance.bat
@@ -1,1 +1,3 @@
-﻿"C:\Program Files\Microsoft SQL Server\120\Tools\Binn\SqlLocalDB.exe" create "ProjectsV12" 12.0 -s
+﻿"C:\Program Files\Microsoft SQL Server\120\Tools\Binn\SqlLocalDB.exe" create "V12.0" 12.0 -s
+sqlcmd -S "(localdb)\V12.0" -Q "EXEC sys.sp_configure N'contained database authentication', N'1';"
+sqlcmd -S "(localdb)\V12.0" -Q "RECONFIGURE WITH OVERRIDE;"
