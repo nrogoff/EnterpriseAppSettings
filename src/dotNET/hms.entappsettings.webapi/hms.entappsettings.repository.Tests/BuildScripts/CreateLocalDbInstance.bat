0f0b903d8262c454647b511b9d247dbd6874d4a2
Nicholas Rogoff <nicholas@rogoff.uk.com>
2017-02-17 10:04:05 +0000

    changed to batch file and default localdb and delay

---
+++ src/dotNET/hms.entappsettings.webapi/hms.entappsettings.repository.Tests/BuildScripts/CreateLocalDbInstance.bat
@@ -0,0 +1,9 @@
+REM == CREATE LocalDb 
+REM "C:\Program Files\Microsoft SQL Server\120\Tools\Binn\SqlLocalDB.exe" create "V12.0" 12.0 -s
+REM --- 5 Sec delay
+REM ping 127.0.0.1 -n 5 > nul
+REM "C:\Program Files\Microsoft SQL Server\120\Tools\Binn\SqlLocalDB.exe" info "V12.0"
+sqlcmd -S "(localdb)\MSSQLLocalDB" -Q "EXEC sys.sp_configure N'contained database authentication', N'1';"
+sqlcmd -S "(localdb)\MSSQLLocalDB" -Q "RECONFIGURE WITH OVERRIDE;"
+REM --- 5 sec delay
+ping 127.0.0.1 -n 5 > nul
--- src/dotNET/hms.entappsettings.webapi/hms.entappsettings.webapi/BuildScripts/CreateLocalDbInstance.bat
+++
@@ -1,4 +0,0 @@
-REM == CREATE LocalDb 
-"C:\Program Files\Microsoft SQL Server\120\Tools\Binn\SqlLocalDB.exe" create "V12.0" 12.0 -s
-sqlcmd -S "(localdb)\V12.0" -Q "EXEC sys.sp_configure N'contained database authentication', N'1';"
-sqlcmd -S "(localdb)\V12.0" -Q "RECONFIGURE WITH OVERRIDE;"
