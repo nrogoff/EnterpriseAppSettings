9d7b015d70ef229a39a138326ca18d1d40bccf12
Nicholas Rogoff <nicholas@rogoff.uk.com>
2017-02-17 10:10:52 +0000

    changed create local db script to only show info

--- src/dotNET/hms.entappsettings.webapi/hms.entappsettings.repository.Tests/BuildScripts/CreateLocalDbInstance.bat
+++ src/dotNET/hms.entappsettings.webapi/hms.entappsettings.repository.Tests/BuildScripts/CreateLocalDbInstance.bat
@@ -1,9 +1,10 @@
 REM == CREATE LocalDb 
 REM "C:\Program Files\Microsoft SQL Server\120\Tools\Binn\SqlLocalDB.exe" create "V12.0" 12.0 -s
+"C:\Program Files\Microsoft SQL Server\120\Tools\Binn\SqlLocalDB.exe" INFO "MSSQLLocalDB"
 REM --- 5 Sec delay
 REM ping 127.0.0.1 -n 5 > nul
 REM "C:\Program Files\Microsoft SQL Server\120\Tools\Binn\SqlLocalDB.exe" info "V12.0"
-sqlcmd -S "(localdb)\MSSQLLocalDB" -Q "EXEC sys.sp_configure N'contained database authentication', N'1';"
-sqlcmd -S "(localdb)\MSSQLLocalDB" -Q "RECONFIGURE WITH OVERRIDE;"
+REM sqlcmd -S "(localdb)\MSSQLLocalDB" -Q "EXEC sys.sp_configure N'contained database authentication', N'1';"
+REM sqlcmd -S "(localdb)\MSSQLLocalDB" -Q "RECONFIGURE WITH OVERRIDE;"
 REM --- 5 sec delay
-ping 127.0.0.1 -n 5 > nul
+REM ping 127.0.0.1 -n 5 > nul
